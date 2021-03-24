16916629df4b54bf7f9cb3673acd119964ce408b
Ethan Chew <ethanchewmh@gmail.com>
2021-03-24 18:14:16 +0800

    Style and Design Changes

--- EncryptDecrypt/app.js
+++ EncryptDecrypt/app.js
@@ -1,9 +1,6 @@
-const cypher = {"a":"4bB*","b":"a3Vf","c":"B3a*","d":"1bHn","e":"a*(6","f":"kBH7","g":"bh52","h":"aP3b","i":"a(2h","j":"pI0a","k":"t(A*","l":"aBeL","m":"R*(n","n":"2bA0","o":"Ar9b","p":"a5h*","q":")(*)","r":"b9a)","s":"2jx9","t":"r94k","u":"2j0q","v":"4a*(","w":"4nq3","x":"24(*","y":"*(yz","z":"lM84"}
-
-const textToEncrypt = toString(document.getElementById("textToEncrypt").innerText)
-const textToDecrypt = toString(document.getElementById("textToDecrypt").innerText)
-
 function encryptText() {
+    const cypher = {"a":"4bB*","b":"a3Vf","c":"B3a*","d":"1bHn","e":"a*(6","f":"kBH7","g":"bh52","h":"aP3b","i":"a(2h","j":"pI0a","k":"t(A*","l":"aBeL","m":"R*(n","n":"2bA0","o":"Ar9b","p":"a5h*","q":")(*)","r":"b9a)","s":"2jx9","t":"r94k","u":"2j0q","v":"4a*(","w":"4nq3","x":"24(*","y":"*(yz","z":"lM84"}
+    const textToEncrypt = toString(document.getElementById("textToEncrypt").innerText)
     var splittedText = textToEncrypt.split(" ")
     var encryptedText = ""
 
@@ -20,6 +17,8 @@ function encryptText() {
 }
 
 function decryptText() {
+    const cypher = {"a":"4bB*","b":"a3Vf","c":"B3a*","d":"1bHn","e":"a*(6","f":"kBH7","g":"bh52","h":"aP3b","i":"a(2h","j":"pI0a","k":"t(A*","l":"aBeL","m":"R*(n","n":"2bA0","o":"Ar9b","p":"a5h*","q":")(*)","r":"b9a)","s":"2jx9","t":"r94k","u":"2j0q","v":"4a*(","w":"4nq3","x":"24(*","y":"*(yz","z":"lM84"}
+    const textToDecrypt = toString(document.getElementById("textToDecrypt").innerText)
     var splittedText = textToDecrypt.split(" ")
     var decryptedText = ""
 
