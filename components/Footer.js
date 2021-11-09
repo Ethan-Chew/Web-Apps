26fd766b5658223cb8f0f6c61e3113b5c672143b
Ethan Chew <50659708+Ethan-Chew@users.noreply.github.com>
2021-11-09 12:14:50 +0800

    Updated Link

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
             <Container as={Stack} maxW={'6xl'} py={4} direction={{ base: 'column', md: 'row' }} spacing={4} justify={{ base: 'center', md: 'space-between' }} align={{ base: 'center', md: 'center' }}>
                 <Text style={{ margin: "25px 0", textAlign: "center", fontSize: 13 }} color="">
                     Made with ♥&#xFE0E; by{" "}
-                    <Link color="blue.200" href="https://ethanchew.me" target="_blank" rel="noreferrer">Ethan Chew</Link>{" "}
+                    <Link color="blue.200" href="https://www.ethanchew.com" target="_blank" rel="noreferrer">Ethan Chew</Link>{" "}
                     <br />
                     Open sourced on{" "}
                     <Link color="blue.200" href="https://github.com/Ethan-Chew/Web-Apps" target="_blank" rel="noreferrer">GitHub</Link>
